537c992c0729e903d863624aea8230475051271d
Camboui <mathias.branger@gmail.com>
2016-02-08 15:40:46 +0100

    udpate

--- client.sh
+++ client.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+param="application/x-rtp, media=(string)video, clock-rate=(int)90000, encoding-name=(string)RAW,"
+param+="sampling=(string)YCbCr-4:4:4, depth=(string)8, width=(string)500, height=(string)500,colorimetry=(string)SMPTE240M,"
+param+="ssrc=(uint)1825678493, payload=(int)96, clock-base=(uint)4068866987, seqnum-base=(uint)24582"
+
+
 if [ "$(uname)" == "Darwin" ]; then
-	gst-launch-1.0 udpsrc caps="application/x-rtp, media=(string)video, clock-rate=(int)90000, encoding-name=(string)RAW, sampling=(string)YCbCr-4:4:4, depth=(string)8, width=(string)500, height=(string)500, colorimetry=(string)SMPTE240M, ssrc=(uint)1825678493, payload=(int)96, clock-base=(uint)4068866987, seqnum-base=(uint)24582" port=9996 ! queue ! rtpvrawdepay  ! queue ! videoconvert ! autovideosink
+	gst-launch-1.0 udpsrc caps="$param" port=9996 ! queue ! rtpvrawdepay  ! queue ! videoconvert ! autovideosink
 elif [ "$(uname)" == "Linux" ]; then
-	gst-launch-1.0 udpsrc caps="application/x-rtp, media=(string)video, clock-rate=(int)90000, encoding-name=(string)RAW, sampling=(string)YCbCr-4:4:4, depth=(string)8, width=(string)500, height=(string)500, colorimetry=(string)SMPTE240M, ssrc=(uint)1825678493, payload=(int)96, clock-base=(uint)4068866987, seqnum-base=(uint)24582" port=9996 ! queue ! rtpvrawdepay  ! queue ! videoconvert ! autovideosink
+	gst-launch-1.0 udpsrc caps="$param" port=9996 ! queue ! rtpvrawdepay  ! queue ! videoconvert ! autovideosink
 fi;
 
