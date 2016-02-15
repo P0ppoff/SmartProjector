453fd02d085b8ccdc2640cda05a841c2e62c7098
Camboui <mathias.branger@gmail.com>
2016-02-15 15:57:41 +0100

    update client/server IPs

--- serveur.sh
+++ serveur.sh
@@ -5,7 +5,7 @@
 
 
 if [ "$(uname)" == "Darwin" ]; then
-		gst-launch-1.0 rtpbin name=rtpbin avfvideosrc capture-screen=true ! videorate ! videoscale ! videoconvert ! 'video/x-raw, format=(string)AYUV, width=(int)500, height=(int)500, framerate=(fraction)30/1' !  rtpvrawpay ! rtpbin ! udpsink clients="192.168.43.173:9996" sync=false async=false udpsrc port=10000 
+		gst-launch-1.0 rtpbin name=rtpbin avfvideosrc capture-screen=true ! videorate ! videoscale ! videoconvert ! 'video/x-raw, format=(string)AYUV, width=(int)500, height=(int)500, framerate=(fraction)30/1' !  rtpvrawpay ! rtpbin ! udpsink clients="192.168.43.247:9996" sync=false async=false udpsrc port=10000 
 elif [ "$(uname)" == "Linux" ]; then
-	gst-launch-1.0 rtpbin name=rtpbin ximagesrc ! videorate ! videoscale ! videoconvert ! 'video/x-raw, format=(string)AYUV, width=(int)500, height=(int)500, framerate=(fraction)30/1' !  rtpvrawpay ! rtpbin ! udpsink clients="192.168.43.247:9996" sync=false async=false udpsrc port=10000 
+	gst-launch-1.0 rtpbin name=rtpbin ximagesrc ! videorate ! videoscale ! videoconvert ! 'video/x-raw, format=(string)AYUV, width=(int)500, height=(int)500, framerate=(fraction)30/1' !  rtpvrawpay ! rtpbin ! udpsink clients="192.168.43.173:9996" sync=false async=false udpsrc port=10000 
 fi;
