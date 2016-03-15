7a494e9dc91c45210204b16c309c37367e4c2ab3
P0ppoff <jules.hablot@gmail.com>
2016-03-15 15:11:46 +0100

    rename project to server

---
+++ gst-multiscreens-serveur/main.cpp
@@ -0,0 +1,13 @@
+#include <QCoreApplication>
+#include "server.h"
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    Server client;
+
+    client.HelloUDP();
+
+    return a.exec();
+}
