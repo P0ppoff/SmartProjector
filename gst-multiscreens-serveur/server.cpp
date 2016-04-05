2ca9e02409665ea286d167c548131f66b44737b2
camboui <mathias.branger@gmail.com>
2016-04-05 11:49:54 +0200

    again

--- gst-multiscreens-serveur/server.cpp
+++ gst-multiscreens-serveur/server.cpp
@@ -204,9 +204,9 @@ void Server::setPipeline()
         {
             if(clients[i].isSending)
             {
-               toLaunch+=" udpsrc port="+ QString::number(clients[i].port)+" caps=\"application/x-rtp, media=video, clock-rate=90000"
-                         ", encoding-name=H264\" ! rtph264depay ! queue ! h264parse ! queue ! omxh264dec ! queue ! videoscale ! video/x-raw "
-                         ", width="+ QString::number(x) +", height="+ QString::number(y) +" ! mix.";
+                toLaunch+=" udpsrc port="+ QString::number(clients[i].port)+" caps=\"application/x-rtp, media=video, clock-rate=90000"
+                          ", encoding-name=H264\" ! rtph264depay ! queue ! h264parse ! queue ! omxh264dec ! queue ! videoscale ! video/x-raw "
+                          ", width="+ QString::number(x) +", height="+ QString::number(y) +" ! mix.";
 
 
                 /*toLaunch+=" udpsrc port="+ QString::number(clients[i].port)+" caps = \"application/x-rtp, media=(string)video, "
@@ -219,7 +219,7 @@ void Server::setPipeline()
     {
         toLaunch="videotestsrc pattern=3 ! textoverlay font-desc=\"Sans 24\" "
                  "text=\"Connect to "+  localIp + ":"+ QString::number(serveur->serverPort()) + "\" shaded-background=true "
-                 "! videoconvert ! video/x-raw , width="+ QString::number(300) +", height="+ QString::number(300) + "! ximagesink sync=false";
+                 "! videoconvert ! video/x-raw , width="+ QString::number(screen.width()) +", height="+ QString::number(screen.height()) + "! ximagesink sync=false";
 
         /*toLaunch="videotestsrc pattern=3 ! textoverlay font-desc=\"Sans 24\" "
                  "text=\"Connect to "+  localIp + ":"+ QString::number(serveur->serverPort()) + "\" shaded-background=true "
